7d61caf4390ec6acc3ec619aba28a167d414b95d
tsots <mosalatsotetsi@gmail.com>
2020-01-20 01:07:23 +0200

    changed routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
 
 import Toolbar from './components/Toolbar/Toolbar';
